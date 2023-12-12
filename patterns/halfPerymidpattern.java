619d41f067de46d43d1571b10d6173c193b90e7f
PrernaBadwane <prernabadwane@gmail.com>
2023-12-12 15:39:37 +0530

    half pyramid  pattern code added

---
+++ patterns/halfPerymidpattern.java
@@ -0,0 +1,32 @@
+package DataStructursAndAlgorithms.patterns;
+
+import java.util.Scanner;
+/*
+pattern to perint
+ 1
+ 12
+ 123
+ 1234
+ 12345
+ 123456
+ */
+
+public class halfPerymidpattern {
+    public static void main(String[] args) {
+        
+        Scanner in=new Scanner(System.in);
+        int lines;
+        System.out.println("enter number of lines : ");
+        lines=in.nextInt();
+        printPatten(lines);
+    }
+
+    public static void  printPatten(int n){
+      for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            System.out.print(j);
+        }
+        System.out.println();
+      }
+    }
+}

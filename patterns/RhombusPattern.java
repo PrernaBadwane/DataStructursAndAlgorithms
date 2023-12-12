53aebaa30ddde9cbdbe4d07e5d314bc3dfabd470
PrernaBadwane <prernabadwane@gmail.com>
2023-12-12 23:22:02 +0530

    Rhombus pattern code added

--- patterns/RhombusPattern.java
+++ patterns/RhombusPattern.java
@@ -1,18 +1,8 @@
 package DataStructursAndAlgorithms.patterns;
 
-/*
-
-     ******
-    ******
-   ******
-  ******
- ******
-******
- 
-*/
 import java.util.Scanner;
 
-public class ButterflyPattern {
+public class RhombusPattern {
     public static void main(String[] args) {
         
         Scanner in=new Scanner(System.in);
@@ -23,20 +13,13 @@ public class ButterflyPattern {
     }
 
     public static void  printPatten(int n){
-      
-      for (int i =1; i <= n; i++) {
-        for (int j = 1; j <=2*n; j++) {
-
-            if(i+j>n && i+j<=2*n){
-                System.out.print("*");
-              }
-              else{
-                  System.out.print(" ");
-              }
+      for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= (2*n)-1; j++) {
+            
+                System.out.print(((i+j)%2==0)?"1":"0");
             
         }
         System.out.println();
       }
     }
-    
 }
